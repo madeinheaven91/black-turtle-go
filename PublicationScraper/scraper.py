dfff2fba9e2d0eb2cd8e7f30bb7d95e3d2efcf53
madeinheaven91 <exspectanss@gmail.com>
2026-01-12 14:01:09 +0300

    remove selenium from scraper

--- PublicationScraper/scraper.py
+++ PublicationScraper/scraper.py
@@ -1,9 +1,6 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import requests
 import json
 import os
-
 
 uuid = os.getenv("UUID")
 if uuid is None:
@@ -11,7 +8,6 @@ if uuid is None:
     print("Usage: UUID={uuid} python3 scraper.py")
     exit(1)
 else:
-    url = "https://schedule.mstimetables.ru/publications/" + uuid
     api_url = "https://schedule.mstimetables.ru/api/publications/" + uuid + "/"
 
 if os.path.exists("groups.csv"):
@@ -21,21 +17,15 @@ else:
 output_file = open("groups.csv", "w")
 output_file.write("api_id,kind,name\n")
 
-driver = webdriver.Firefox()
-
-driver.get(url + "/#/groups")
-
-groups = driver.find_elements(By.CLASS_NAME, "link")
-
+resp = requests.get(api_url + "groups")
+groups = json.loads(resp.text)
 for group in groups:
-    name = group.text
-    url = group.get_attribute("href")
-    id = url.split("/")[7]
+    name = group["name"]
+    id = str(group["id"])
     print(id + ",group," + name)
     output_file.write(id + ",group," + name + "\n")
 
 output_file.close()
-driver.close()
 
 if os.path.exists("teachers.csv"):
     print("found teachers.csv, removing")
@@ -48,12 +38,12 @@ output_file.write("api_id,kind,name\n")
 
 resp = requests.get(api_url + "teachers")
 teachers = json.loads(resp.text)
-for teacher in teachers:
-    name = teacher["fio"]
+for group in teachers:
+    name = group["fio"]
     # name = teacher["fio"].split(" ")
     # while len(name) < 3:
     #     name.append("")
-    id = str(teacher["id"])
+    id = str(group["id"])
 
     print(id + ",teacher," + name)
     output_file.write(id + ",teacher," + name + "\n")
